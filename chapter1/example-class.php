2eea5bb29eec7c84c1a74a56303f44f01668774f
yUm1k4 <zaiabdullah99@gmail.com>
2021-06-12 20:40:00 +0700

    feat: chapter 1 'cara mendefinisikan  class'

---
+++ chapter1/example-class.php
@@ -0,0 +1,19 @@
+<?php
+
+class Student
+{
+}
+
+// cek ada kelas apa saja
+$classes = get_declared_classes();
+echo "Classes: " . implode(', ', $classes) . "<br>";
+
+$classNames = ['Product', 'Student', 'student'];
+
+foreach ($classNames as $cn) {
+    if (class_exists($cn)) {
+        echo "{$cn} class ada. <br>";
+    } else {
+        echo "{$cn} class tidak ada <br>";
+    }
+}
